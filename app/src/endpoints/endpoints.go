77d2d93fb75ce97f0b97ee9bab42ee3ae1c4f89d
Dvorfin <dvorfin@yandex.ru>
2023-08-30 22:11:51 +0300

    initial commit ^)

---
+++ app/src/endpoints/endpoints.go
@@ -0,0 +1,238 @@
+package endpoints
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var addr string = "172.10.0.2:6379"
+var pass string = "avito"
+
+type Response struct {
+	Key   string `json: "key"`
+	Value string `json: "value"`
+}
+
+type InfoResponse struct {
+	Msg string `json: "msg"`
+}
+
+type ShowData struct { // структура для вывода информации на главной странице
+	GetKeyMsg string
+	SetKeyMsg string
+	DelKeyMsg string
+}
+
+func get_key(w http.ResponseWriter, r *http.Request) { // функция обработки эндпоинта /get_key
+	fmt.Printf("got /get_key connection \n")
+
+	key := r.URL.Query().Get("key")
+
+	cert, err := tls.LoadX509KeyPair("tests/tls/redis.crt", "tests/tls/redis.key")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	caCert, err := os.ReadFile("tests/tls/ca.crt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	client := redis.NewClient(&redis.Options{
+		//Addr: "redis:6379",
+		Addr:     addr,
+		Password: pass,
+		TLSConfig: &tls.Config{
+			MinVersion:   tls.VersionTLS12,
+			Certificates: []tls.Certificate{cert},
+			RootCAs:      caCertPool,
+			//ServerName: "Generic-cert",
+			ServerName: "localhost",
+		},
+		DB: 0,
+	})
+
+	ctx := context.Background()
+
+	val, err := client.Get(ctx, key).Result()
+	if err != nil {
+		infoResp := InfoResponse{Msg: "There is no such key."}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(404)                  // если такого ключа нет в редисе, то 404
+		json.NewEncoder(w).Encode(infoResp) // возвращаем json
+		//http.NotFound(w, r)
+
+		return
+	}
+
+	res := Response{Key: key, Value: val}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)             // ресурс найден 200 ок
+	json.NewEncoder(w).Encode(res) // возвращаем json
+
+}
+
+func set_key(w http.ResponseWriter, r *http.Request) { // функция обработки эндпоинта /set_key
+	fmt.Printf("got /set_key connection \n")
+
+	var res Response
+	err := json.NewDecoder(r.Body).Decode(&res) // получаем json инфу переданную в body и записываем в res
+
+	//fmt.Println(res)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest) // ошибка отправленных данных
+		infoResp := InfoResponse{Msg: "Invalid key or value type! Must be string."}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(infoResp) // возвращаем json
+		return
+	}
+
+	cert, err := tls.LoadX509KeyPair("tests/tls/redis.crt", "tests/tls/redis.key")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	caCert, err := os.ReadFile("tests/tls/ca.crt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: pass,
+		TLSConfig: &tls.Config{
+			MinVersion:   tls.VersionTLS12,
+			Certificates: []tls.Certificate{cert},
+			RootCAs:      caCertPool,
+			ServerName:   "localhost",
+		},
+		DB: 0,
+	})
+
+	ctx := context.Background()
+
+	// fmt.Println(res.Key, res.Value) полученные ключ и значение
+
+	response_string := "Key setted."
+
+	val, err := client.Get(ctx, res.Key).Result() // проверяем есть ли ключ в БД
+	if val != "" {                                // если есть то пишем, что обновили
+		response_string = "Key updated."
+	}
+
+	err = client.Set(ctx, res.Key, res.Value, 0).Err() // устанавливаем значение по ключу
+	if err != nil {
+		panic(err)
+	}
+
+	var empty_value string
+
+	if res.Value == "" { // если не передано значение ключа, то добавляем сообщение об этом
+		empty_value = "Notice, that value was empty!"
+	}
+
+	infoResp := InfoResponse{Msg: fmt.Sprintf("%s %s", response_string, empty_value)} // формируем json ответ
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+	json.NewEncoder(w).Encode(infoResp) // возвращаем json
+}
+
+func del_key(w http.ResponseWriter, r *http.Request) { // функция обработки эндпоинта /del_key
+	fmt.Printf("got /del_key connection \n")
+
+	var res Response
+	err := json.NewDecoder(r.Body).Decode(&res) // получаем json инфу переданную в body и записываем в res
+
+	//fmt.Println(res)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest) // ошибка отправленных данных
+		infoResp := InfoResponse{Msg: "Invalid key type! Must be string."}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(infoResp) // возвращаем json
+		return
+	}
+
+	cert, err := tls.LoadX509KeyPair("tests/tls/redis.crt", "tests/tls/redis.key")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	caCert, err := os.ReadFile("tests/tls/ca.crt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	client := redis.NewClient(&redis.Options{
+		Addr: addr,
+		//Addr:     "redis:6379",
+		Password: pass,
+		TLSConfig: &tls.Config{
+			MinVersion:   tls.VersionTLS12,
+			Certificates: []tls.Certificate{cert},
+			RootCAs:      caCertPool,
+			//ServerName: "Generic-cert",
+			ServerName: "localhost",
+		},
+		DB: 0,
+	})
+
+	ctx := context.Background()
+	key := res.Key
+
+	_, err = client.Get(ctx, key).Result()
+
+	if err != nil { // если такого ключа нет в БД, то ошибку
+		infoResp := InfoResponse{Msg: "There is no such key."}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(404)
+		json.NewEncoder(w).Encode(infoResp) // возвращаем json
+		return
+	}
+
+	err = client.Del(ctx, res.Key).Err() // удаляем данные по ключу
+	if err != nil {
+		panic(err)
+	}
+
+	infoResp := InfoResponse{Msg: "Key sucessfully deleted!"}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+	json.NewEncoder(w).Encode(infoResp) // возвращаем json
+
+}
+
+func Check_endpoint(r *http.Request) bool {
+	url := html.EscapeString(r.URL.Path)
+	if url != "/" {
+		return false
+	}
+	return true
+}
+
+func HandleRequest() {
+	http.HandleFunc("/get_key", get_key)
+	http.HandleFunc("/set_key", set_key)
+	http.HandleFunc("/del_key", del_key)
+	//htpp.HandleFunc("/get_handle", get_handle)
+	// http.HandleFunc("/create", create)
+	// http.HandleFunc("/save_data", save_data)
+	http.ListenAndServe(":8080", nil)
+}
---
+++ app/src/main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	endpnts "gitgub.com/my/repo/endpoints"
+)
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	//fmt.Println(r.FormValue("key_get"))
+
+	// проверка, что переход только по разрешенным эндпоинтам
+	if !endpnts.Check_endpoint(r) {
+		infoResp := endpnts.InfoResponse{Msg: "Forbidden"}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(403)                  // на все некорректные uri отвечаем 403
+		json.NewEncoder(w).Encode(infoResp) // возвращаем json
+		return
+	}
+
+	//http.NotFound(w, r)
+
+	//fmt.Println(r.URL)
+
+	var show_data endpnts.ShowData
+
+	var s string
+
+	if key := r.FormValue("key_get"); key != "" { // если хотим получить данные
+		req, _ := http.NewRequest("GET", fmt.Sprintf("http://localhost:8080/get_key?key=%s", key), nil) // то делаем запрос
+
+		resp, _ := http.DefaultClient.Do(req)
+
+		defer resp.Body.Close()
+		body, _ := io.ReadAll(resp.Body)
+
+		s = string(body)
+		show_data.GetKeyMsg = s
+		fmt.Printf("Get response: %s", body)
+
+	}
+
+	if key := r.FormValue("key_set"); key != "" { // если хотим добавить данные
+
+		val := r.FormValue("value")
+
+		fmt.Println(key, val)
+
+		data := endpnts.Response{Key: key, Value: val}
+		var buf bytes.Buffer
+		err := json.NewEncoder(&buf).Encode(data)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		req, _ := http.NewRequest("POST", "http://localhost:8080/set_key", &buf) // то делаем запрос
+		req.Header.Add("accept", "application/json")
+		req.Header.Add("content-type", "application/json")
+
+		resp, _ := http.DefaultClient.Do(req)
+		defer resp.Body.Close()
+		body, _ := io.ReadAll(resp.Body)
+
+		s = string(body)
+		show_data.SetKeyMsg = s
+		fmt.Printf("Set response: %s", body)
+
+	}
+
+	if key := r.FormValue("key_del"); key != "" { // если хотим добавить данные
+
+		data := endpnts.Response{Key: key}
+		var buf bytes.Buffer
+		err := json.NewEncoder(&buf).Encode(data)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		req, _ := http.NewRequest("DELETE", "http://localhost:8080/del_key", &buf) // то делаем запрос
+		req.Header.Add("accept", "application/json")
+		req.Header.Add("content-type", "application/json")
+
+		resp, _ := http.DefaultClient.Do(req)
+		defer resp.Body.Close()
+		body, _ := io.ReadAll(resp.Body)
+
+		s = string(body)
+
+		show_data.DelKeyMsg = s
+
+		//fmt.Println(body)
+		fmt.Printf("Delete response: %s", body)
+
+	}
+
+	tmpl, err := template.ParseFiles("templates/index.html")
+
+	if err != nil { // если получили ошибку то выводим на страницу какую
+		fmt.Fprintf(w, err.Error())
+	}
+
+	if s != "" {
+		tmpl.Execute(w, show_data)
+	} else {
+		tmpl.Execute(w, nil)
+	}
+
+}
+
+func without_interface(w http.ResponseWriter, r *http.Request) {
+	// проверка, что переход только по разрешенным эндпоинтам
+	if !endpnts.Check_endpoint(r) { //
+		infoResp := endpnts.InfoResponse{Msg: "Forbidden"}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(403)                  // на все некорректные uri отвечаем 403
+		json.NewEncoder(w).Encode(infoResp) // возвращаем json
+		return
+	}
+	fmt.Fprintf(w, "<h1>App is running!</h1>")
+}
+
+func main() {
+
+	//fmt.Println(len(os.Args), os.Args[1]) // считываем аргумент при запуске
+
+	if len(os.Args) > 1 && os.Args[1] == "yes" { // если да, то запускаем с веб-интерфейсом
+		fmt.Println("App is running with web-interface!")
+		http.HandleFunc("/", home_page)
+	} else { // если не переданы аргументы или передан другой, то запуск только для curl запросов
+		fmt.Println("App is running!")
+		http.HandleFunc("/", without_interface)
+	}
+
+	endpnts.HandleRequest()
+
+}
